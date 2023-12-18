3b43de36cbf85b10d946410a5bca4e2aa0bf585a
kylian <aahf969@gmail.com>
2023-12-18 14:59:31 +0100

    travail-php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ include './view/view.php';
 $message = "";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submitArticle'])) {
-    $nom_article = filter_input(INPUT_POST, 'nom_article', FILTER_SANITIZE_STRING);
-    $prix_article = filter_input(INPUT_POST, 'prix_article', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    $nom_article = filter_input(INPUT_POST, 'nom_article');
+    $prix_article = filter_input(INPUT_POST, 'prix_article');
 
     if ($nom_article && $prix_article) {
         $message = ajouterArticle($nom_article, $prix_article);
